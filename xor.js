9616e38e31f50593dcbad344b2f0aa29f5fb6175
Danilo Collares <dundeepessoal@gmail.com>
2019-12-23 01:52:50 -0300

    + one time pad cryptography added

---
+++ xor.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto')
+
+function oneTimePadEncrypt( data ) {
+	let pad = crypto.randomBytes( data.length );
+	let result = new Array( data.length )
+	console.log( data, pad ); let i = 0
+	for ( let character of data ){
+		result[i] = character.charCodeAt(0) ^ pad[i]
+		i++;
+	} 
+	return {result: result, pad: pad};
+}
+
+function oneTimePadDecrypt( encryptedData, pad ) {
+	let i = 0; let decrypt = new Array( encryptedData.length );
+	for ( let character of encryptedData ){
+		decrypt[i] = encryptedData[i] ^ pad[i];
+		decrypt[i] = String.fromCharCode(decrypt[i])
+		i++;
+	} decrypt = decrypt.join('');
+	return {decryptedData: decrypt};
+}
+
+export { oneTimePadEncrypt, oneTimePadDecrypt }
